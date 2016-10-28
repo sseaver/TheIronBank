fec070ca0ae9ab090ea729e7a7db52df493cb21e
sseaver <sseaver321@gmail.com>
2016-10-28 12:08:59 -0400

    added skeleton framework

--- TheIronBank/urls.py
+++ TheIronBank/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from app.views import IndexView, UserCreateView, TransactionListAPIView, TransactionDetailUpdateDestroyAPIView
+from app.views import (IndexView, UserCreateView, TransactionListAPIView, TransactionDetailUpdateDestroyAPIView,
+                       ProfileView)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),
+    url('^accounts/profile/$', ProfileView.as_view(), name="profile_view"),
     url(r'^$', IndexView.as_view(), name='index_view'),
     url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
     url(r'^api/transactions/$', TransactionListAPIView.as_view(), name="transaction_list_api_view"),
--- app/views.py
+++ app/views.py
@@ -3,20 +3,29 @@ from app.models import Account, Transaction
 from app.serializers import TransactionSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
-from django.views import View
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic import CreateView, TemplateView
 # Create your views here.
 
 
-class IndexView(View):
-    pass
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['login_form'] = AuthenticationForm
+        return context
 
 
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = "/"
+
+
+class ProfileView(CreateView):
+    model = Transaction
+    fields = "__all__"
 
 
 class TransactionListAPIView(ListCreateAPIView):
